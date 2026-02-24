3209d7bf2d5955940a72855e6eda7b9e25bb4066
Akhil <gaddeakhil01@gmail.com>
2026-02-24 18:41:32 +0530

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { CartProvider } from './stores/context/CartContext.jsx'
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 <BrowserRouter>
