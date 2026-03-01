19216231b1a94b45e1c8bc937f98dd2b78521060
gaddeakhil <gaddeakhil01@gmail.com>
2026-03-01 15:14:11 +0530

    Update Men.jsx

--- src/stores/components/Men.jsx
+++ src/stores/components/Men.jsx
@@ -21,7 +21,7 @@ const Men = () => {
               <Link to="/men">
                 <img
                   className="card-img-top img-fluid"
-                  src={item.image}
+                   src={${import.meta.env.BASE_URL}${item.image}}
                   alt={`Men Fashion ${index + 1}`}
                   style={{
                     objectFit: "cover",   // fills box edge-to-edge
