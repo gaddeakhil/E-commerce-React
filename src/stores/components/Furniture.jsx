4e52f3ac02e5047142a037ac2c99b598873f138f
gaddeakhil <gaddeakhil01@gmail.com>
2026-03-01 15:13:23 +0530

    Update Furniture.jsx

--- src/stores/components/Furniture.jsx
+++ src/stores/components/Furniture.jsx
@@ -21,7 +21,7 @@ const Furniture = () => {
               <Link to="/furniture">
                 <img
                   className="card-img-top img-fluid"
-                  src={item.image}
+                  src={${import.meta.env.BASE_URL}${item.image}}
                   alt={`Furniture ${index + 1}`}
                   style={{
                     objectFit: "cover",   // fills box edge-to-edge
