7eb525ce8ccea337e570e3f197accffc8f189de9
gaddeakhil <gaddeakhil01@gmail.com>
2026-03-01 15:15:10 +0530

    Update Woman.jsx

--- src/stores/components/Woman.jsx
+++ src/stores/components/Woman.jsx
@@ -21,7 +21,7 @@ const Woman = () => {
               <Link to="/woman">
                 <img
                   className="card-img-top img-fluid"
-                  src={item.image}
+                  src={${import.meta.env.BASE_URL}${item.image}}
                   alt={`Woman Dressing ${index + 1}`}
                   style={{
                     objectFit: "cover",   // fills box edge-to-edge
