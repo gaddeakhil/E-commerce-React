b01ad5be04446a34769efcc12e616077b8ce83c6
gaddeakhil <gaddeakhil01@gmail.com>
2026-03-01 15:13:07 +0530

    Update Fridge.jsx

--- src/stores/components/Fridge.jsx
+++ src/stores/components/Fridge.jsx
@@ -21,7 +21,7 @@ const Fridge = () => {
               <Link to="/fridge">
                 <img
                   className="card-img-top img-fluid"
-                  src={item.image}
+                 src={${import.meta.env.BASE_URL}${item.image}}
                   alt={`Fridge ${index + 1}`}
                   style={{
                     objectFit: "cover",   // fills box edge-to-edge
